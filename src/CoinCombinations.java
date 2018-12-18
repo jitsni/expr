b1f8080da4b8d76737b257d636a1bc3805e49ebd
Jitendra Kotamraju <jitsni@gmail.com>
2018-12-17 20:33:51 -0800

    Indentation

--- src/CoinCombinations.java
+++ src/CoinCombinations.java
@@ -17,15 +17,15 @@ print:
 import java.util.*;
 
 public class CoinCombinations {
-	public static void main(String ... args) {
-		Set<Integer> sums = new TreeSet<>();
-		dfs(10, 15, 55, 0, sums);
-		System.out.println(sums);
+    public static void main(String ... args) {
+        Set<Integer> sums = new TreeSet<>();
+        dfs(10, 15, 55, 0, sums);
+        System.out.println(sums);
 
-		printSums(10, 15, 55);
-	}
+        printSums(10, 15, 55);
+    }
 
-	static void printSums(int c1, int c2, int c3) {
+    static void printSums(int c1, int c2, int c3) {
 
         Set<Integer> sums = new HashSet<>();
         sums.add(0);
@@ -39,16 +39,16 @@ public class CoinCombinations {
         }
     }
 
-	static void dfs(int c1, int c2, int c3, int sum, Set<Integer> sums) {
-		if (sum > 1000) return;	
-		if (sums.contains(sum)) return;
+    static void dfs(int c1, int c2, int c3, int sum, Set<Integer> sums) {
+        if (sum > 1000) return;    
+        if (sums.contains(sum)) return;
 
-		if (sum != 0) {
-			sums.add(sum);
-		}
+        if (sum != 0) {
+            sums.add(sum);
+        }
 
-		dfs(c1, c2, c3, sum + c1, sums);
-		dfs(c1, c2, c3, sum + c2, sums);
-		dfs(c1, c2, c3, sum + c3, sums);
-	}
+        dfs(c1, c2, c3, sum + c1, sums);
+        dfs(c1, c2, c3, sum + c2, sums);
+        dfs(c1, c2, c3, sum + c3, sums);
+    }
 }
