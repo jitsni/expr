357f80c43a0aaa0c3f6faf11d351542ad84b6294
Jitendra Kotamraju <jitsni@gmail.com>
2019-01-30 20:49:40 -0800

    indentation

--- src/InterleaveStrings.java
+++ src/InterleaveStrings.java
@@ -8,30 +8,30 @@ could be merged. Figure out the formula F(m, n) = ?
 */
 
 /*
-	(m+n)!
+    (m+n)!
     ------
     m! n!
 */
 
 public class InterleaveStrings {
 
-	public static void main(String ... args) {
-		List<String> all = new ArrayList<>();
-		combinations("123", 0, "45", 0, "", all);
-		System.out.println(all);
-	}
+    public static void main(String ... args) {
+        List<String> all = new ArrayList<>();
+        combinations("123", 0, "45", 0, "", all);
+        System.out.println(all);
+    }
 
-	private static void combinations(String str1, int i, String str2, int j, String current, List<String> all) {
-		if (current.length() == str1.length() + str2.length()) {
-			all.add(current);
-			return;
-		}
-		if (i < str1.length()) {
-			combinations(str1, i+1, str2, j, current + str1.charAt(i), all);
-		}
-		if (j < str2.length()) {
-			combinations(str1, i, str2, j + 1, current + str2.charAt(j), all);
-		}
-	}
+    private static void combinations(String str1, int i, String str2, int j, String current, List<String> all) {
+        if (current.length() == str1.length() + str2.length()) {
+            all.add(current);
+            return;
+        }
+        if (i < str1.length()) {
+            combinations(str1, i+1, str2, j, current + str1.charAt(i), all);
+        }
+        if (j < str2.length()) {
+            combinations(str1, i, str2, j + 1, current + str2.charAt(j), all);
+        }
+    }
 
 }
